2973617b3a0dc831056d79102df12a8a32f20253
= <=>
2021-10-05 21:46:46 -0300

    First commit

--- projetoAcademia/Conexao.cs
+++ projetoAcademia/Conexao.cs
@@ -9,5 +9,14 @@ namespace projetoAcademia
 {
     class Conexao
     {
+        public SQLiteConnection Con { get; set; }
+        public SQLiteConnection open()
+        {
+            string caminho = Environment.CurrentDirectory + "\\academia.sqlite";
+            string sql = string.Format("Data Source = {0}, version=3", caminho);
+            Con = new SQLiteConnection(sql);
+            Con.Open();
+            return Con;
+        }
     }
 }
